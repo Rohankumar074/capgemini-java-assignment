208c1777b6dc670d5414647a5b5b245fab948bf8
Rohan kumar <115989256+Rohankumar074@users.noreply.github.com>
2026-02-26 15:59:31 +0530

    hibernate

--- hibernate_many_to_one/src/main/java/com/capgemini/hibernate_many_to_one/App.java
+++ hibernate_many_to_one/src/main/java/com/capgemini/hibernate_many_to_one/App.java
@@ -1,5 +1,6 @@
 package com.capgemini.hibernate_many_to_one;
 
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -33,3 +34,4 @@ public class App {
 		}
 	}
 }
+
